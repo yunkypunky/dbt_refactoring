6e955fde60518d7f868f5a5371e33f88c65924cc
yunki@aykc.com <yunki@aykc.com>
2022-01-13 06:07:15 +0000

    more cleanup

--- models/core/fct_customer_orders.sql
+++ models/core/fct_customer_orders.sql
@@ -19,12 +19,12 @@ customers as (
 paid_orders as (
 
     select 
-        order_id, 
-        any_value(order_date) as order_placed_at,
-        any_value(customer_id) as customer_id,
-        any_value(order_status) as order_status,
-        max(created_date) as payment_finalized_date, 
-        sum(amount) as total_amount_paid
+        payments.order_id, 
+        any_value(orders.order_date) as order_placed_at,
+        any_value(orders.customer_id) as customer_id,
+        any_value(orders.order_status) as order_status,
+        max(payments.created_date) as payment_finalized_date, 
+        sum(payments.amount) as total_amount_paid
     from payments
     left join orders using (order_id)
     where payment_status <> 'fail'
@@ -35,23 +35,23 @@ paid_orders as (
 final as (
 
     select 
-        po.order_id,
-        customer_id,
-        order_placed_at,
-        order_status,
-        total_amount_paid,
-        payment_finalized_date,
-        customer_first_name,
-        customer_last_name,
-        row_number() over (order by po.order_id) as transaction_seq,
-        row_number() over (partition by customer_id order by po.order_id) as customer_sales_seq,
+        paid_orders.order_id,
+        paid_orders.customer_id,
+        paid_orders.order_placed_at,
+        paid_orders.order_status,
+        paid_orders.total_amount_paid,
+        paid_orders.payment_finalized_date,
+        customers.customer_first_name,
+        customers.customer_last_name,
+        row_number() over (order by paid_orders.order_id) as transaction_seq,
+        row_number() over (partition by paid_orders.customer_id order by paid_orders.order_id) as customer_sales_seq,
         case
-        when rank() over(partition by customer_id order by order_placed_at) = 1 then 'new'
+        when rank() over(partition by paid_orders.customer_id order by paid_orders.order_placed_at) = 1 then 'new'
             else 'return'
         end as nvsr,
-        sum(total_amount_paid) over(partition by customer_id order by order_id) as customer_lifetime_value,
-        min(order_placed_at) over(partition by customer_id order by order_placed_at) as fdos
-    from paid_orders as po
+        sum(paid_orders.total_amount_paid) over(partition by paid_orders.customer_id order by paid_orders.order_id) as customer_lifetime_value,
+        min(paid_orders.order_placed_at) over(partition by paid_orders.customer_id order by paid_orders.order_placed_at) as fdos
+    from paid_orders
     join customers using (customer_id)
     order by order_id
 
