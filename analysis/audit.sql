4d53856ecca2c1a14cdb45f3109062debc9a257c
yunki@aykc.com <yunki@aykc.com>
2022-01-14 10:16:50 +0000

    second fct_customer_orders

--- analysis/audit.sql
+++ analysis/audit.sql
@@ -1,5 +1,5 @@
 {% set old_etl_relation=ref('customer_orders') %}
-{% set dbt_relation=ref('fct_customer_orders') %}
+{% set dbt_relation=ref('fct_customer_orders2') %}
 
 {{ audit_helper.compare_relations(
     a_relation=old_etl_relation,
